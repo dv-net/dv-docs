8e6d9bf93ef1ba7fa853e3efb98e10e568a7d0c2
Daniel Parker <daniel.parker@dv.net>
2025-12-11 12:01:37 +0300

    Dv 3975 (#16)
    * dv-3974
    
    * dv-3975

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,10 +1,29 @@
 import {URL, fileURLToPath} from 'node:url'
-import {defineConfig, HeadConfig, loadEnv} from 'vitepress'
+import {defineConfig, loadEnv} from 'vitepress'
 import {locales} from "./locales.js";
 
 const env = loadEnv('', process.cwd())
-const gTagId = env.VITE_GTAG_ID
-const gTagIdAnalytics = env.VITE_GTAG_ID_ANALYTICS
+const isProduction = env.VITE_NODE_ENV === 'production'
+const gtagId = env.VITE_GTAG_ID
+
+const head: any[] = [
+  ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+  ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },],
+  ['link', { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap', rel: 'stylesheet' }],
+]
+
+// Add Google Tag Manager if production and GTM ID is set
+if (isProduction && gtagId) {
+  head.push([
+    'script',
+    {},
+    `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtagId}');`
+  ])
+}
 
 export default defineConfig({
   title: "DV.net Docs",
@@ -12,48 +31,16 @@ export default defineConfig({
   srcDir: 'src',
   themeConfig: { nav: [{ component: 'LocaleSelect' }] },
   locales,
-
-  head: [
-    // Google Fonts
-    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
-    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },],
-    ['link', { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap', rel: 'stylesheet' }],
-    // logo and favicon
-    // ['link', { rel: 'icon', href: '/favicon.ico' }],
-    // Google Analytics
-    ...(gTagIdAnalytics
-        ? [[
-          'script',
-          {},
-          `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${gTagIdAnalytics}');`,
-        ],
-          [
-            'noscript',
-            {},
-            `<iframe src="https://www.googletagmanager.com/ns.html?id=${gTagIdAnalytics}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-          ]]
-
-        : [['script', { async: '', src: `https://www.googletagmanager.com/gtag/js?id=${gTagId}` }],
-          [
-            'script',
-            {},
-            `window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gTagId}');`,
-          ]]
-    ) as HeadConfig[]
-  ],
-
+  head,
   vite: {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./', import.meta.url)),
       },
+    },
+    define: {
+      'import.meta.env.VITE_NODE_ENV': JSON.stringify(env.VITE_NODE_ENV || 'development'),
+      'import.meta.env.VITE_GTAG_ID': JSON.stringify(env.VITE_GTAG_ID || ''),
     },
   },
 })
---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+
+const isProduction = import.meta.env.VITE_NODE_ENV === 'production'
+const gtagId = import.meta.env.VITE_GTAG_ID
+</script>
+
+<template>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript v-if="isProduction && gtagId">
+    <iframe :src="`https://www.googletagmanager.com/ns.html?id=${gtagId}`" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  </noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <Layout />
+</template>
